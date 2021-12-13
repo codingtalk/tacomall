d00d24d7d218742d12a03fd92562fb076a3ee913
码上talk|RC <3189482282@qq.com>
2021-12-13 08:27:49 +0800

    1、优化调度器

--- tacomall-api/job/executor/src/main/java/store/tacomall/jobexecutor/JobExecutorApplication.java
+++ tacomall-api/job/executor/src/main/java/store/tacomall/jobexecutor/JobExecutorApplication.java
@@ -1,21 +1,36 @@
 /***
  * @Author: 码上talk|RC/3189482282@qq.com
  * @Date: 2021-10-10 16:45:46
- * @LastEditTime: 2021-10-15 16:51:36
+ * @LastEditTime: 2021-12-13 08:27:22
  * @LastEditors: 码上talk|RC
  * @Description: 
- * @FilePath: /tacomall-api/job/executor/src/java/store/tacomall/jobexecutor/JobExecutorApplication.java
+ * @FilePath: /tacomall-api/job/executor/src/main/java/store/tacomall/jobexecutor/JobExecutorApplication.java
  */
 package store.tacomall.jobexecutor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+import store.tacomall.common.aspect.NoRepeatSubmitAspect;
+import store.tacomall.common.config.RedisConfig;
+import store.tacomall.common.config.WebMvcCorsConfig;
+import store.tacomall.common.config.WebMvcInterceptorConfig;
+import store.tacomall.common.config.WxMaConfig;
+import store.tacomall.common.config.WxPayConfiguration;
+import store.tacomall.common.libs.wx.WxPayUtil;
+import store.tacomall.common.util.RedisUtil;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "store.tacomall.common", "store.tacomall.jobexecutor" }, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { WxPayUtil.class, WxPayConfiguration.class,
+                WxMaConfig.class, NoRepeatSubmitAspect.class, RedisUtil.class, RedisConfig.class,
+                WebMvcInterceptorConfig.class, WebMvcCorsConfig.class }) })
 public class JobExecutorApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(JobExecutorApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(JobExecutorApplication.class, args);
+    }
 
 }
--- tacomall-ma/mixins/type/pageInfo.js
+++ tacomall-ma/mixins/type/pageInfo.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 码上talk|RC
  * @Date: 2020-06-09 23:20:26
- * @LastEditTime: 2021-10-28 15:31:14
+ * @LastEditTime: 2021-12-03 14:55:06
  * @LastEditors: 码上talk|RC
  * @Description:
  * @FilePath: /tacomall-ma/mixins/type/pageInfo.js
@@ -9,37 +9,36 @@
  * @邮箱:  3189482282@qq.com
  * @oops: Just do what I think it is right
  */
-import Vue from 'vue';
 import { model } from '@/model';
 
 const { Request } = model;
 
 export default {
-  data () {
-    return {
-      PAGE_INFO: {}
-    }
-  },
-  methods: {
-    _boot () {
-      if (!this.PAGE_CONFIG) {
-        this.$log4js.error('attr "PAGE_CONFIG" is require');
-      }
-      return new Request().do('ma', 'page', 'info', { params: { key: this.PAGE_CONFIG.key }, data: this.PAGE_CONFIG.reqData || {} }).then(res => {
-        const { status, data } = res;
-        if (status) {
-          this.PAGE_INFO = data;
-          this.PAGE_CONFIG.callback && this.PAGE_CONFIG.callback();
-        } else {
-          this.toast('获取页面信息失败');
+    data () {
+        return {
+            PAGE_INFO: {}
         }
-      });
     },
-    infoGet (s, noneValue = '--') {
-      return this.$_.get(this.PAGE_INFO, s, noneValue);
+    methods: {
+        _boot () {
+            if (!this.PAGE_CONFIG) {
+                this.$log4js.error('attr "PAGE_CONFIG" is require');
+            }
+            return new Request().do('ma', 'page', 'info', { params: { key: this.PAGE_CONFIG.key }, data: this.PAGE_CONFIG.reqData || {} }).then(res => {
+                const { status, data } = res;
+                if (status) {
+                    this.PAGE_INFO = data;
+                    this.PAGE_CONFIG.callback && this.PAGE_CONFIG.callback();
+                } else {
+                    this.toast('获取页面信息失败');
+                }
+            });
+        },
+        infoGet (s, noneValue = '--') {
+            return this.$_.get(this.PAGE_INFO, s, noneValue);
+        }
+    },
+    onLoad () {
+        this._boot();
     }
-  },
-  onLoad () {
-    this._boot();
-  }
 };
